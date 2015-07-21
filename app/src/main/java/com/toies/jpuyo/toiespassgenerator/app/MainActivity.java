0004107db953f68bbde29fdf7cf42842499dea31
jpuyo <jpgpuyo@msn.com>
2015-07-22 01:52:31 +0200

    When app starts, database is populated (in foreground)

--- app/src/main/java/com/toies/jpuyo/toiespassgenerator/app/MainActivity.java
+++ app/src/main/java/com/toies/jpuyo/toiespassgenerator/app/MainActivity.java
@@ -1,8 +1,15 @@
 package com.toies.jpuyo.toiespassgenerator.app;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+
+import com.toies.jpuyo.toiespassgenerator.app.data.JsonPlayerParser;
+import com.toies.jpuyo.toiespassgenerator.app.data.PlayerContract;
+
+import org.json.JSONException;
 
 
 public class MainActivity extends ActionBarActivity implements PlayerFragment.Callback {
@@ -10,6 +17,7 @@ public class MainActivity extends ActionBarActivity implements PlayerFragment.Ca
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        populateDatabase();
         setContentView(R.layout.activity_main);
         getSupportActionBar().setElevation(0f);
 
@@ -20,5 +28,22 @@ public class MainActivity extends ActionBarActivity implements PlayerFragment.Ca
     @Override
     public void onItemSelected(Uri dateUri) {
 
+    }
+
+    private void populateDatabase() {
+        this.getContentResolver().delete(
+                PlayerContract.PlayerEntry.CONTENT_URI,
+                null,
+                null
+        );
+
+        JsonPlayerParser jsonPlayerParser;
+        try {
+            jsonPlayerParser = new JsonPlayerParser(this);
+            ContentValues[] bulkInsertContentValues = jsonPlayerParser.getContentValues();
+            this.getContentResolver().bulkInsert(PlayerContract.PlayerEntry.CONTENT_URI, bulkInsertContentValues);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
