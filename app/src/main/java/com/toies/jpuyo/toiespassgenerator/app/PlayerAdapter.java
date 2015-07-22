ea552b0070498e247b93b756b222e8392976f978
jpuyo <jpgpuyo@msn.com>
2015-07-22 19:04:34 +0200

    Removed view type from PlayerAdapter

--- app/src/main/java/com/toies/jpuyo/toiespassgenerator/app/PlayerAdapter.java
+++ app/src/main/java/com/toies/jpuyo/toiespassgenerator/app/PlayerAdapter.java
@@ -8,18 +8,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-/**
- * {@link PlayerAdapter} exposes a list of Players
- * from a {@link Cursor} to a {@link android.widget.ListView}.
- */
 public class PlayerAdapter extends CursorAdapter {
 
-    private static final int VIEW_TYPE_COUNT = 1;
-    private static final int VIEW_TYPE_FUTURE_DAY = 1;
-
-    /**
-     * Cache of the children views for a forecast list item.
-     */
     public static class ViewHolder {
 
         public final TextView descriptionView;
@@ -35,21 +25,9 @@ public class PlayerAdapter extends CursorAdapter {
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        // Choose the layout type
-        int viewType = getItemViewType(cursor.getPosition());
-        int layoutId = -1;
-        switch (viewType) {
-            case VIEW_TYPE_FUTURE_DAY: {
-                layoutId = R.layout.list_item_player;
-                break;
-            }
-        }
-
-        View view = LayoutInflater.from(context).inflate(layoutId, parent, false);
-
+        View view = LayoutInflater.from(context).inflate(R.layout.list_item_player, parent, false);
         ViewHolder viewHolder = new ViewHolder(view);
         view.setTag(viewHolder);
-
         return view;
     }
 
@@ -60,15 +38,5 @@ public class PlayerAdapter extends CursorAdapter {
 
         String name = cursor.getString(PlayerFragment.COL_NAME);
         viewHolder.descriptionView.setText(name);
-    }
-
-    @Override
-    public int getItemViewType(int position) {
-        return VIEW_TYPE_FUTURE_DAY;
-    }
-
-    @Override
-    public int getViewTypeCount() {
-        return VIEW_TYPE_COUNT;
     }
 }
