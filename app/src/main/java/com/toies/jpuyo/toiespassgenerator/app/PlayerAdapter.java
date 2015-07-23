f85bf38904b65f6346e01e2d7739978708471d33
jpuyo <jpgpuyo@msn.com>
2015-07-23 05:04:41 +0200

    Added dummy functionality to button GetPassword

--- app/src/main/java/com/toies/jpuyo/toiespassgenerator/app/PlayerAdapter.java
+++ app/src/main/java/com/toies/jpuyo/toiespassgenerator/app/PlayerAdapter.java
@@ -1,13 +1,17 @@
 package com.toies.jpuyo.toiespassgenerator.app;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.toies.jpuyo.toiespassgenerator.app.data.PlayerContract;
 
 public class PlayerAdapter extends CursorAdapter {
 
@@ -40,8 +44,31 @@ public class PlayerAdapter extends CursorAdapter {
         ViewHolder viewHolder = (ViewHolder) view.getTag();
 
         String name = cursor.getString(PlayerFragment.COL_NAME);
-        viewHolder.iconPasswordUsed.setImageResource(R.drawable.ic_fog);
+        long playerRowId = cursor.getInt(PlayerFragment.COL_ID);
+        int passwordUsed = cursor.getInt(PlayerFragment.COL_USED);
+
+        if (passwordUsed == 1){
+            viewHolder.iconPasswordUsed.setImageResource(R.drawable.ic_clear);
+        }else{
+            viewHolder.iconPasswordUsed.setImageResource(R.drawable.ic_light_rain);
+        }
         viewHolder.playerName.setText(name);
+
+        Button getPasswordButton = (Button) view.findViewById(R.id.list_item_player_btn_get_password);
+        getPasswordButton.setTag(playerRowId);
+        getPasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                long playerRowId = (long)v.getTag();
+
+                ContentValues updatedValues = new ContentValues();
+                updatedValues.put(PlayerContract.PlayerEntry.USED, 1);
+
+                mContext.getContentResolver().update(
+                        PlayerContract.PlayerEntry.CONTENT_URI, updatedValues, PlayerContract.PlayerEntry._ID + "= ?",
+                        new String[] { Long.toString(playerRowId)});
+            }
+        });
 
     }
 }
